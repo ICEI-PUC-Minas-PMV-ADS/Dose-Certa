9ae84720b1310131be382b79d9bc2eff01da512c
Rafaelxr <83379675+Rafaelxr@users.noreply.github.com>
2024-03-12 12:23:33 -0300

    Add files via upload

---
+++ api-web-services-dose-certa/api-web-services-dose-certa/Models/Visita.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace APIDoseCerta.Models
+{
+    public class Visita
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public String Observação { get; set; }
+
+       
+    }
+}
