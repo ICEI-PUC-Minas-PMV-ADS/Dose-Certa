25de08bafc79d859e1b333c3481e79bd472d91b1
ianevictoria <ianevictoria.tec@gmail.com>
2024-04-02 12:58:54 -0300

    logica completa de medicacoes

--- api-web-services-dose-certa/api-web-services-dose-certa/Models/Medicacoes.cs
+++ api-web-services-dose-certa/api-web-services-dose-certa/Models/Medicacoes.cs
@@ -6,6 +6,8 @@ namespace APIDoseCerta.Medicacoes
     {
         public int Id { get; set; }
         public DateTime DateInicio { get; set; }
+
+        [Required]
         public DateTime DateFim { get; set; }
         public String Observacao { get; set; }
     }
