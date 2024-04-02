4b084ebc78949e7253e99fe82084f33d6433e7e2
Rafaelxr <83379675+Rafaelxr@users.noreply.github.com>
2024-04-02 10:32:00 -0300

    Update Medicamentos.cs
    mudamos o modelo do projeto, e medicação se tornou uma classe com objetivo diferente

--- api-web-services-dose-certa/api-web-services-dose-certa/Models/Medicamentos.cs
+++ api-web-services-dose-certa/api-web-services-dose-certa/Models/Medicamentos.cs
@@ -1,16 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace APIDoseCerta.Models
 {
-    public class Medicamento
+    public class Medicacao
     {
         public int Id { get; set; }
-        [Required]
-        public string Name { get; set; }
-        [Required]
-        public String Dosagem { get; set; }
-        [Required]
-        public String Indicacoes { get; set; }
-
+        public DateTime DataInicio { get; set; }
+        public DateTime DataTermino { get; set; }
+        public string Observacao { get; set; }
     }
+
 }
