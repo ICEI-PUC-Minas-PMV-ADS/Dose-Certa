ea44a0c891bef78b89d7db13e9fedbc532e8e009
Rafaelxr <83379675+Rafaelxr@users.noreply.github.com>
2024-04-02 10:33:43 -0300

    Add files via upload

---
+++ api-web-services-dose-certa/api-web-services-dose-certa/Controllers/VisitaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using APIDoseCerta.Models;
+
+namespace APIDoseCerta.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VisitaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
