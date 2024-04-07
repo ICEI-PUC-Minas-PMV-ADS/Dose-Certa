5af4f193562543562615cd42b030f7f0b176c414
joicepassos <joicepassos72@gmail.com>
2024-04-07 12:57:49 -0300

    Finalização crud de usuário com mysql

--- api-web-services-dose-certa/api-web-services-dose-certa/Models/User.cs
+++ api-web-services-dose-certa/api-web-services-dose-certa/Models/User.cs
@@ -10,7 +10,7 @@ namespace APIDoseCerta.Models
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
