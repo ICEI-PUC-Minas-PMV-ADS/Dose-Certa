75be89e64d931dd105a0f9d8f8f0e888fd62aca2
Milleny Ellen Jodas Ferreira <ellenmilleny@gmail.com>
2024-04-28 20:01:26 -0300

    branch milleny

--- api-web-services-dose-certa/api-web-services-dose-certa/obj/Debug/net8.0/api-web-services-dose-certa.AssemblyInfo.cs
+++ api-web-services-dose-certa/api-web-services-dose-certa/obj/Debug/net8.0/api-web-services-dose-certa.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api-web-services-dose-certa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6afa2332d2dcfdba5d3539ba3c82e3bb8f4b9eb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+152fd1e436404f6162e05a55c83a4b120f941a2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api-web-services-dose-certa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api-web-services-dose-certa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
