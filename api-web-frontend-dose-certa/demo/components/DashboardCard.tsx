55605d73e65192363d0a1ae52ada4a97d3bbfe75
callmebyiris <irisg4690@gmail.com>
2024-05-05 15:23:36 -0300

    (refactor): Centralizando o card de pacientes

--- api-web-frontend-dose-certa/demo/components/DashboardCard.tsx
+++ api-web-frontend-dose-certa/demo/components/DashboardCard.tsx
@@ -1,32 +1,27 @@
-
 import { Card } from 'primereact/card';
 import { Button } from "primereact/button";
 import { Calendar } from "primereact/calendar";
 
 const DashboardCard = () => {
     return (
-        <div style={{ display: 'flex'}}>
-            <div>
-            <Card style={{ width: '364px', height: '127px', marginTop: '20px',marginBottom: '20px' }}>
-                <p className="m-0">
-                    <div style={{justifyItems:"center",}}>
-                        <i className="pi pi-user" style={{ fontSize: '3rem', color: '#66' }}></i>
-                        <span>Total de Pacientes</span>
-                        <p>35</p>
-                    </div>
-                </p>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <div style={{ marginRight: '20px' }}>
+                <Card style={{ width: '364px', height: '127px', marginTop: '20px', marginBottom: '20px' }}>
+                    <p className="m-0">
+                        <div style={{ justifyContent: "center" }}>
+                            <i className="pi pi-user" style={{ fontSize: '3rem', color: '#66' }}></i>
+                            <span>Total de Pacientes</span>
+                            <p>35</p>
+                        </div>
+                    </p>
                 </Card>
             </div>
-            <div style={{ display:"grid", marginTop: "20px",  marginLeft:"20px" }}>
-                <div className="p-inputgroup flex-1">
-                <input type="date"id="dateInput" name="date" />
+            <div>
+                <div className="p-inputgroup flex-1" style={{ marginBottom: '20px' }}>
+                    <input type="date" id="dateInput" name="date" />
                 </div>
-
-                <div className="p-inputgroup flex-1">
-
-
-                <input type="date" id="dateInput" name="date" />
-
+                <div className="p-inputgroup flex-1" style={{ marginBottom: '20px' }}>
+                    <input type="date" id="dateInput" name="date" />
                 </div>
                 <div>
                     <Button>Filtrar</Button>
@@ -35,4 +30,5 @@ const DashboardCard = () => {
         </div>
     )
 }
+
 export default DashboardCard;
