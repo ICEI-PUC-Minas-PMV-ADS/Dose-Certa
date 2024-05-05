3a8fd983583a53523a75e868b7efe528918eaed6
callmebyiris <irisg4690@gmail.com>
2024-05-05 16:25:09 -0300

    (refactor): Posicionando o filtro ao lado da dashboard e estilizando

--- api-web-frontend-dose-certa/demo/components/Charts.tsx
+++ api-web-frontend-dose-certa/demo/components/Charts.tsx
@@ -1,31 +1,43 @@
-import { Chart } from "react-google-charts"
+import { Chart } from "react-google-charts";
 import DashboardCard from "./DashboardCard";
-
+import { Calendar } from "primereact/calendar";
+import { Button } from "primereact/button";
 
 export const data = [
     ["Element", "Quantidade", { role: "style" }],
     ["Rémedios Registrados", 8, "#0471DA"], // RGB value
     ["Medicações Registradas", 30, "#22B9C3"], // English color name
     ["Visitas Agendadas", 19, "#76A6D4"],
-
-  ];
+];
 
 const Charts = () => {
     return (
-        <>
-        <div className="col-12 border-bottom-2 p-2">
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <div className="col-12 border-bottom-2 p-2">
                 <h2>Visão Geral</h2>
             </div>
-            <div>
-                <DashboardCard/>
+            <div style={{ marginBottom: '20px' }}>
+                <DashboardCard />
             </div>
-
-        <div>
-             <Chart chartType="ColumnChart" width="100%" height="400px" data={data} />
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', marginTop: '10px' }}>
+                <div style={{ flex: 1 }}>
+                    <Chart chartType="ColumnChart" width="100%" height="400px" data={data} />
+                </div>
+                <div style={{ marginLeft: '20px', display: 'flex', flexDirection: 'column', alignItems: 'center', backgroundColor: 'white', padding: '20px', borderRadius: '5px' }}>
+                    <h4 style={{ marginBottom: '30px' }}>Filtre por período</h4>
+                    <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '30px' }}>
+                        <label htmlFor="startDate" style={{ marginBottom: '8px' }}>Data Inicial</label>
+                        <Calendar id="startDate" placeholder="dd/mm/aaaa" style={{ marginRight: '10px' }}></Calendar>
+                    </div>
+                    <div style={{ display: 'flex', flexDirection: 'column', marginBottom: '10px' }}>
+                        <label htmlFor="endDate" style={{ marginBottom: '8px' }}>Data Final</label>
+                        <Calendar id="endDate" placeholder="dd/mm/aaaa" style={{ marginRight: '10px' }}></Calendar>
+                    </div>
+                    <Button icon="pi pi-filter" label="Filtrar" className="p-button-success" style={{ backgroundColor: '#0C84F3', marginTop: '30px' }}></Button>
+                </div>
+            </div>
         </div>
-        </>
+    );
+}
 
-)}
-
-
-export default Charts
+export default Charts;
--- api-web-frontend-dose-certa/demo/components/DashboardCard.tsx
+++ api-web-frontend-dose-certa/demo/components/DashboardCard.tsx
@@ -1,31 +1,20 @@
 import { Card } from 'primereact/card';
-import { Button } from "primereact/button";
-import { Calendar } from "primereact/calendar";
 
 const DashboardCard = () => {
     return (
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
             <div style={{ marginRight: '20px' }}>
-                <Card style={{ width: '364px', height: '127px', marginTop: '20px', marginBottom: '20px' }}>
-                    <p className="m-0">
-                        <div style={{ justifyContent: "center" }}>
+                <Card style={{ width: '364px', height: '127px', marginTop: '30px', marginBottom: '10px' }}>
+                    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
+                        <div style={{ marginRight: '15px' }}>
                             <i className="pi pi-user" style={{ fontSize: '3rem', color: '#66' }}></i>
-                            <span>Total de Pacientes</span>
-                            <p>35</p>
                         </div>
-                    </p>
+                        <div style={{ textAlign: 'center' }}>
+                            <span style={{ fontSize: '1.2rem', marginBottom: '0.5rem' }}>Total de Pacientes</span>
+                            <p style={{ fontSize: '1.5rem', margin: 0, fontWeight: 'bold' }}>35</p>
+                        </div>
+                    </div>
                 </Card>
-            </div>
-            <div>
-                <div className="p-inputgroup flex-1" style={{ marginBottom: '20px' }}>
-                    <input type="date" id="dateInput" name="date" />
-                </div>
-                <div className="p-inputgroup flex-1" style={{ marginBottom: '20px' }}>
-                    <input type="date" id="dateInput" name="date" />
-                </div>
-                <div>
-                    <Button>Filtrar</Button>
-                </div>
             </div>
         </div>
     )
